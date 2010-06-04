c8cc30275019facdc284a7b12db3baaf3541f9dc
Wysie <sohyuanchin@gmail.com>
2010-06-04 15:24:46 +0800

    Added code to export to XML. Also displays toast messages when appropriate now.

--- apps/SpareParts/src/com/android/spare_parts/SpareParts.java
+++ apps/SpareParts/src/com/android/spare_parts/SpareParts.java
@@ -52,6 +52,10 @@ import java.io.IOException;
 import org.xmlpull.v1.XmlPullParserException;
 import android.graphics.Color;
 import android.widget.Toast;
+import java.io.FileWriter;
+import org.xmlpull.v1.XmlSerializer;
+import android.util.Xml;
+import java.util.ArrayList;
 
 public class SpareParts extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener,
@@ -97,8 +101,9 @@ public class SpareParts extends PreferenceActivity
     private static final String UI_NOTIF_ITEM_TIME_COLOR = "notifications_time_color";
     private static final String UI_RESET_TO_DEFAULTS = "reset_ui_tweaks_to_defaults";
     private static final String UI_IMPORT_FROM_XML = "import_from_xml";
+    private static final String UI_EXPORT_TO_XML = "export_to_xml";
     
-    private static final String IMPORT_FILENAME = "spare_parts_ui.xml";
+    private static final String XML_FILENAME = "spare_parts_ui.xml";
     
     private final Configuration mCurConfig = new Configuration();
     
@@ -137,6 +142,7 @@ public class SpareParts extends PreferenceActivity
     
     private Preference mResetToDefaults;
     private Preference mImportFromXML;
+    private Preference mExportToXML;
 
     private IWindowManager mWindowManager;
 
@@ -230,6 +236,7 @@ public class SpareParts extends PreferenceActivity
         mNotifItemTimePref = prefSet.findPreference(UI_NOTIF_ITEM_TIME_COLOR);
         mResetToDefaults = prefSet.findPreference(UI_RESET_TO_DEFAULTS);
         mImportFromXML = prefSet.findPreference(UI_IMPORT_FROM_XML);
+        mExportToXML = prefSet.findPreference(UI_EXPORT_TO_XML);
         
         if (!isSwapEnabled()) {
             prefSet.removePreference(mCompcachePref);
@@ -277,19 +284,19 @@ public class SpareParts extends PreferenceActivity
                     Settings.Secure.COMPCACHE_ENABLED, 0) != 0);
             mShowClockPref.setChecked(Settings.System.getInt(
                     getContentResolver(),
-                    Settings.System.SHOW_STATUS_CLOCK, 0) != 0);
+                    Settings.System.SHOW_STATUS_CLOCK, 1) != 0);
             mShowPlmnLsPref.setChecked(Settings.System.getInt(
                     getContentResolver(),
-                    Settings.System.SHOW_PLMN_LS, 0) != 0);
+                    Settings.System.SHOW_PLMN_LS, 1) != 0);
             mShowSpnLsPref.setChecked(Settings.System.getInt(
                     getContentResolver(),
-                    Settings.System.SHOW_SPN_LS, 0) != 0);
+                    Settings.System.SHOW_SPN_LS, 1) != 0);
             mShowPlmnSbPref.setChecked(Settings.System.getInt(
                     getContentResolver(),
-                    Settings.System.SHOW_PLMN_SB, 0) != 0);
+                    Settings.System.SHOW_PLMN_SB, 1) != 0);
             mShowSpnSbPref.setChecked(Settings.System.getInt(
                     getContentResolver(),
-                    Settings.System.SHOW_SPN_SB, 0) != 0);
+                    Settings.System.SHOW_SPN_SB, 1) != 0);
     }
     
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -417,12 +424,14 @@ public class SpareParts extends PreferenceActivity
             AlertDialog alertDialog = new AlertDialog.Builder(this).create();
             alertDialog.setTitle(getResources().getString(R.string.title_dialog_ui_interface));
             alertDialog.setMessage(getResources().getString(R.string.message_dialog_reset));
-            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), 
+                new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                     resetUITweaks();
                 }
             });
-            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), 
+            new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                     
                 }
@@ -435,12 +444,14 @@ public class SpareParts extends PreferenceActivity
             AlertDialog alertDialog = new AlertDialog.Builder(this).create();
             alertDialog.setTitle(getResources().getString(R.string.title_dialog_ui_interface));
             alertDialog.setMessage(getResources().getString(R.string.message_dialog_import));
-            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), 
+            new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                     readUIValuesFromXML();
                 }
             });
-            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), 
+            new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                     
                 }
@@ -448,6 +459,26 @@ public class SpareParts extends PreferenceActivity
             alertDialog.show();
             
             return true;
+        }
+        else if (preference == mExportToXML) {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle(getResources().getString(R.string.title_dialog_ui_interface));
+            alertDialog.setMessage(getResources().getString(R.string.message_dialog_export));
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), 
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    writeUIValuesToXML();
+                }
+            });
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), 
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    
+                }
+            });
+            alertDialog.show();
+            
+            return true;        
         }
         
         return false;
@@ -482,18 +513,21 @@ public class SpareParts extends PreferenceActivity
         mShowSpnLsPref.setChecked(true);
         mShowPlmnSbPref.setChecked(true);
         mShowSpnSbPref.setChecked(true);
+        
+        Toast.makeText(getApplicationContext(), R.string.reset_ui_success, Toast.LENGTH_SHORT).show();
     }
     
     private void readUIValuesFromXML() {
         if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Toast.makeText(getApplicationContext(), R.string.xml_sdcard_unmounted, Toast.LENGTH_SHORT).show();
             return;
-        }    
+        }
         
         FileReader reader = null;
         boolean success = false;
                 
         try {
-            reader = new FileReader(new File(Environment.getExternalStorageDirectory() + "/" + IMPORT_FILENAME));
+            reader = new FileReader(new File(Environment.getExternalStorageDirectory() + "/" + XML_FILENAME));
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser parser = factory.newPullParser();
             parser.setInput(reader);
@@ -536,9 +570,119 @@ public class SpareParts extends PreferenceActivity
         
         if (success) {
             Toast.makeText(getApplicationContext(), R.string.xml_import_success, Toast.LENGTH_SHORT).show();
-        }
+        } 
+    }
+    
+    private void writeUIValuesToXML() {
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Toast.makeText(getApplicationContext(), R.string.xml_sdcard_unmounted, Toast.LENGTH_SHORT).show();
+            return;
+        }        
+        
+        FileWriter writer = null;
+        File outFile = new File(Environment.getExternalStorageDirectory() + "/" + "tempfile.xml");
+        boolean success = false;
+        
+        try {            
+            outFile.createNewFile();
+            writer = new FileWriter(outFile);
+            XmlSerializer serializer = Xml.newSerializer();
             
-           
+            ArrayList<String> elements = new ArrayList<String>();
+            elements.add(Settings.System.BATTERY_PERCENTAGE_STATUS_COLOR);
+            elements.add(Settings.System.CLOCK_COLOR);
+            elements.add(Settings.System.DATE_COLOR);
+            elements.add(Settings.System.PLMN_LABEL_COLOR);
+            elements.add(Settings.System.SPN_LABEL_COLOR);
+            elements.add(Settings.System.NEW_NOTIF_TICKER_COLOR);
+            elements.add(Settings.System.NOTIF_COUNT_COLOR);
+            elements.add(Settings.System.NO_NOTIF_COLOR);
+            elements.add(Settings.System.CLEAR_BUTTON_LABEL_COLOR);
+            elements.add(Settings.System.ONGOING_NOTIF_COLOR);
+            elements.add(Settings.System.LATEST_NOTIF_COLOR);
+            elements.add(Settings.System.NOTIF_ITEM_TITLE_COLOR);
+            elements.add(Settings.System.NOTIF_ITEM_TEXT_COLOR);
+            elements.add(Settings.System.NOTIF_ITEM_TIME_COLOR);
+            
+            serializer.setOutput(writer);
+            serializer.startDocument("UTF-8", true);
+            serializer.startTag("", "spareparts");
+            
+            int color = -1;
+            
+            for (String s : elements) {
+                try {
+                    color = Settings.System.getInt(getContentResolver(), s);
+                }
+                catch (SettingNotFoundException e) {
+                    if (s.equals(Settings.System.BATTERY_PERCENTAGE_STATUS_COLOR) ||
+                            s.equals(Settings.System.NOTIF_COUNT_COLOR) ||
+                            s.equals(Settings.System.NO_NOTIF_COLOR) ||
+                            s.equals(Settings.System.ONGOING_NOTIF_COLOR) ||
+                            s.equals(Settings.System.LATEST_NOTIF_COLOR)) {
+                        color = -1;
+                    }
+                    else {
+                        color = -16777216;
+                    }
+                }
+                
+                serializer.startTag("", s);
+                serializer.text(convertToARGB(color));
+                serializer.endTag("", s);
+            }
+            
+            serializer.endTag("", "spareparts");
+            serializer.endDocument();
+            serializer.flush();
+            success = true;
+        }
+        catch (Exception e) {
+            android.util.Log.d("WYSIE", e.toString());
+            Toast.makeText(getApplicationContext(), R.string.xml_write_error, Toast.LENGTH_SHORT).show();
+        }
+        finally {
+            if (writer != null) {
+        		try {
+	        	    writer.close();
+	        	} catch (IOException e) {
+	        	}
+	        }
+        }
+        
+        if (success) {
+            File xmlFile = new File(Environment.getExternalStorageDirectory() + "/" + XML_FILENAME);
+            outFile.renameTo(xmlFile);
+            Toast.makeText(getApplicationContext(), R.string.xml_export_success, Toast.LENGTH_SHORT).show();
+        }
+        
+        if (outFile.exists())
+            outFile.delete();
+    }
+    
+    private String convertToARGB(int color) {
+        String alpha = Integer.toHexString(Color.alpha(color));
+        String red = Integer.toHexString(Color.red(color));
+        String green = Integer.toHexString(Color.green(color));
+        String blue = Integer.toHexString(Color.blue(color));        
+        
+        if (alpha.length() == 1) {
+            alpha = "0" + alpha;
+        }
+        
+        if (red.length() == 1) {
+            red = "0" + red;
+        }
+        
+        if (green.length() == 1) {
+            green = "0" + green;
+        }
+        
+        if (blue.length() == 1) {
+            blue = "0" + blue;
+        }
+        
+        return "#" + alpha + red + green + blue;
     }
 
     public void writeAnimationPreference(int which, Object objValue) {
