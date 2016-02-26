171957c0bdc547ae6013daddffc03c8ec3ac2e3e
Mete Polat <metepolat2000@gmail.com>
2016-02-26 16:42:51 +0100

    idegen: Check if environment variable exists
    Check if the $ANDROID_HOST_OUT variable exists in the environment
    
    Change-Id: I1086f34188f306ad4b864c7461b3f5cd263a89a7

--- tools/idegen/idegen.sh
+++ tools/idegen/idegen.sh
@@ -5,9 +5,14 @@ if [ ! -d development ]; then
     exit 1
 fi
 
+if [ -z "$ANDROID_HOST_OUT" ]; then
+    echo "Couldn't find host out directory. Make sure ANDROID_HOST_OUT is in your environment."
+    exit 127
+fi
+
 idegenjar=`find $ANDROID_HOST_OUT -name idegen.jar -follow | grep -v intermediates`
-if [ "" = "$idegenjar" ]; then
-    echo "Couldn't find idegen.jar. Please run make first."
-else 
+if [ -z "$idegenjar" ]; then
+    echo "Couldn't find idegen.jar. Please run 'make idegen' first."
+else
     java -cp $idegenjar Main
 fi
