45ee56458d2037c7c98ec1959076c2b3e7ae4554
Priyanka <priyankaspatel@google.com>
2025-11-17 11:13:58 +0000

    Adapt VideoFrameCache to act as LRU cache.
    Bug: 455935655
    Test: npm run test:unit:ci
    Change-Id: I67d85da8f578e5c14dbd38864633abe13641a152

--- tools/winscope/src/viewers/common/playback/video_frame_cache_test.ts
+++ tools/winscope/src/viewers/common/playback/video_frame_cache_test.ts
@@ -17,10 +17,14 @@
 import {getFixtureFile} from 'test/unit/io_helpers';
 import {createVideoFrameCache} from './video_frame_cache_factory';
 import {VideoFrameCache} from './video_frame_cache';
+import {PlaybackState} from './playback_state';
 
 describe('VideoFrameCache', () => {
   let dataOneKeyFrameAndRotation: Uint8Array;
   let dataThreeKeyFramesNoRotation: Uint8Array;
+
+  let cacheOneKeyFrameAndRotation: VideoFrameCache;
+  let cacheThreeKeyFramesNoRotation: VideoFrameCache;
 
   beforeAll(async () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
@@ -28,45 +32,127 @@ describe('VideoFrameCache', () => {
       'traces/elapsed_and_real_timestamp/screen_recording_metadata_v3.mp4',
     );
     dataOneKeyFrameAndRotation = new Uint8Array(await file1.arrayBuffer());
+    cacheOneKeyFrameAndRotation = await createVideoFrameCache(
+      dataOneKeyFrameAndRotation,
+    );
 
     const file2 = await getFixtureFile(
       'traces/elapsed_and_real_timestamp/screen_recording_no_metadata.mp4',
     );
     dataThreeKeyFramesNoRotation = new Uint8Array(await file2.arrayBuffer());
+    cacheThreeKeyFramesNoRotation = await createVideoFrameCache(
+      dataThreeKeyFramesNoRotation,
+    );
+  });
+
+  beforeEach(() => {
+    cacheOneKeyFrameAndRotation.reset();
+    cacheThreeKeyFramesNoRotation.reset();
   });
 
   it('throws error if retrieval attempted out of bounds', async () => {
-    const cache = await createVideoFrameCache(dataOneKeyFrameAndRotation);
-    await expectAsync(cache.get(-1)).toBeRejected();
-    await expectAsync(cache.get(105)).toBeRejected();
+    await expectAsync(
+      cacheOneKeyFrameAndRotation.get(-1, PlaybackState.FORWARDS),
+    ).toBeRejected();
+    await expectAsync(
+      cacheOneKeyFrameAndRotation.get(105, PlaybackState.FORWARDS),
+    ).toBeRejected();
   });
 
-  it('retrieves all samples from mp4 with one key frame', async () => {
-    const cache = await createVideoFrameCache(dataOneKeyFrameAndRotation);
-    await checkFrames(cache, 105, 90);
+  it('retrieves all samples with one key frame', async () => {
+    await checkFrameRetrieval(cacheOneKeyFrameAndRotation, 0, 105, 90);
   });
 
-  it('retrieves all samples from mp4 with multiple key frames', async () => {
-    const cache = await createVideoFrameCache(dataThreeKeyFramesNoRotation);
-    await checkFrames(cache, 158, 0);
+  it('retrieves samples starting middle of single key frame chunk', async () => {
+    await checkFrameRetrieval(cacheOneKeyFrameAndRotation, 31, 70, 90);
   });
 
-  async function checkFrames(
+  it('retrieves all samples with multiple key frames', async () => {
+    await checkFrameRetrieval(cacheThreeKeyFramesNoRotation, 0, 158, 0);
+  });
+
+  it('retrieves samples starting from middle of key frame chunk', async () => {
+    await checkFrameRetrieval(cacheThreeKeyFramesNoRotation, 40, 90, 0);
+  });
+
+  it('retrieves samples starting from last key frame chunk', async () => {
+    await checkFrameRetrieval(cacheThreeKeyFramesNoRotation, 60, 122, 0);
+  });
+
+  it('retrieves samples starting from different places in same chunk - forwards', async () => {
+    await checkFrameRetrievalForwards(cacheOneKeyFrameAndRotation, 0, 5, 90);
+    await checkFrameRetrievalForwards(cacheOneKeyFrameAndRotation, 80, 85, 90);
+    await checkFrameRetrievalForwards(cacheOneKeyFrameAndRotation, 40, 45, 90);
+  });
+
+  it('retrieves samples starting from different places in same chunk - backwards', async () => {
+    await checkFrameRetrievalBackwards(cacheOneKeyFrameAndRotation, 80, 85, 90);
+    await checkFrameRetrievalBackwards(cacheOneKeyFrameAndRotation, 0, 5, 90);
+    await checkFrameRetrievalBackwards(cacheOneKeyFrameAndRotation, 40, 45, 90);
+  });
+
+  async function checkFrameRetrieval(
     cache: VideoFrameCache,
-    length: number,
-    expectedAngle: number,
+    first: number,
+    last: number,
+    angle: number,
   ) {
-    const entries = await Promise.all(
-      Array.from({length}, (_, i) => cache.get(i)),
-    );
+    await checkFrameRetrievalForwards(cache, first, last, angle);
+    cache.reset();
+    await checkFrameRetrievalBackwards(cache, first, last, angle);
+  }
+
+  async function checkFrameRetrievalForwards(
+    cache: VideoFrameCache,
+    first: number,
+    last: number,
+    angle: number,
+  ) {
     let prevFrame: ImageBitmap | undefined;
-    for (const entry of entries) {
-      const {frame, rotationAngle} = entry;
-      expect(rotationAngle).toEqual(expectedAngle);
+    for (let i = first; i < last; i++) {
+      const frame = await retrieveFrame(
+        cache,
+        i,
+        angle,
+        PlaybackState.FORWARDS,
+      );
       if (prevFrame) {
         expect(frame === prevFrame).toBeFalse();
       }
       prevFrame = frame;
     }
+  }
+
+  async function checkFrameRetrievalBackwards(
+    cache: VideoFrameCache,
+    first: number,
+    last: number,
+    angle: number,
+  ) {
+    let prevFrame: ImageBitmap | undefined;
+    for (let i = last - 1; i >= first; i--) {
+      const frame = await retrieveFrame(
+        cache,
+        i,
+        angle,
+        PlaybackState.BACKWARDS,
+      );
+      if (prevFrame) {
+        expect(frame === prevFrame).toBeFalse();
+      }
+      prevFrame = frame;
+    }
+  }
+
+  async function retrieveFrame(
+    cache: VideoFrameCache,
+    i: number,
+    expectedAngle: number,
+    state: PlaybackState,
+  ): Promise<ImageBitmap> {
+    const entry = await cache.get(i, state);
+    const {frame, rotationAngle} = entry;
+    expect(rotationAngle).toEqual(expectedAngle);
+    return frame;
   }
 });
