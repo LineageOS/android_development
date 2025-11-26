482961236deaee78b2e64a84152c9e02182ef022
Charisee Chiw <chiw@google.com>
2025-11-26 19:46:52 +0000

    Add license case to cargo_embargo
    Test: mm
    Bug: b/464020536
    Change-Id: Ie6992a20b273a6934e796187e851aec37f26590d

--- tools/cargo_embargo/src/main.rs
+++ tools/cargo_embargo/src/main.rs
@@ -814,6 +814,7 @@ fn choose_licenses(license: &str) -> Result<Vec<&str>> {
         // Usually we interpret "/" as "OR", but in the case of libfuzzer-sys, closer
         // inspection of the terms indicates the correct interpretation is "(MIT OR APACHE) AND NCSA".
         "MIT/Apache-2.0/NCSA" => vec!["Apache-2.0", "NCSA"],
+        "(MIT OR Apache-2.0) AND Unicode-3.0" => vec!["Apache-2.0", "Unicode-3.0"],
 
         // Variations on "Apache-2.0 AND BSD-*"
         "Apache-2.0 AND BSD-3-Clause" => vec!["Apache-2.0", "BSD-3-Clause"],
