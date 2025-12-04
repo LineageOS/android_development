e2e7c76d037c487b048c0131c0044c2e56c83f0c
James Farrell <jamesfarrell@google.com>
2025-12-04 22:20:59 +0000

    Fix directory name on Darwin.
    Test: treehugger
    Change-Id: Ib72888433737fe2f8916ad8e01cc90b8efc72e44

--- tools/external_crates/android_cargo.py
+++ tools/external_crates/android_cargo.py
@@ -23,7 +23,7 @@ import platform
 def main() -> None:
   platform_dir = "linux-x86"
   if sys.platform == "darwin":
-      platform_dir = "darwin-x86"
+      platform_dir = "darwin"
 
   prebuilt_bin = (
       (
