aeb0ffc8af3cdfaae2df025614fdcfc3fd002570
Priyanka <priyankaspatel@google.com>
2026-01-05 20:44:21 +0000

    Timeline component changes after trace pipeline refactor.
    Bug: 465724932
    Test: npm run test:unit:ci
    Change-Id: Ie3f5f8b7f2262d482c65eb19f7e2246e9b06bb8f

--- tools/winscope/src/app/components/timeline/playback_events.ts
+++ tools/winscope/src/app/components/timeline/playback_events.ts
@@ -17,7 +17,7 @@
 import {TraceType} from '@trace_api/trace_type';
 import {PlaybackState} from '@viewers/common/playback/playback_state';
 import {WinscopeEvent} from '@messaging/winscope_event';
-import {TraceGeometryData} from '@parsers/trace_geometry_data';
+import {TraceGeometryData} from '@parsers/helpers/trace_geometry_data';
 
 export class PlaybackStateChangeRequest implements WinscopeEvent {
   constructor(
