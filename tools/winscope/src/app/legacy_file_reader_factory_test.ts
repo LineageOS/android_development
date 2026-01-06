a2caaa30a105d6da7809f7114fc30bacac739cc8
Priyanka <priyankaspatel@google.com>
2026-01-06 14:47:36 +0000

    Parser/Reader factory tests.
    Bug: 465724932
    Test: npm run test:unit:ci
    Change-Id: If6be0f4e10cd31fffb4b8ada0cb06daa7ab677c0

--- tools/winscope/src/app/legacy_file_reader_factory_test.ts
+++ tools/winscope/src/app/legacy_file_reader_factory_test.ts
@@ -15,17 +15,11 @@
  */
 import {LegacyFileReaderFactory} from '@app/legacy_file_reader_factory';
 import {getFixtureFile} from '@test/unit/io_helpers';
-import {
-  timestampEqualityTester,
-  UTC_CONVERTER,
-} from '@test/unit/time_test_helpers';
+import {UTC_CONVERTER} from '@test/unit/time_test_helpers';
 import {TraceFile} from '@trace/trace_file';
+import {TraceType} from '@trace_api/trace_type';
 
 describe('LegacyFileReaderFactory', () => {
-  beforeAll(() => {
-    jasmine.addCustomEqualityTester(timestampEqualityTester);
-  });
-
   describe('is robust to', () => {
     it('empty trace file', async () => {
       await checkRobustToFile('invalid_files/empty.pb', true);
@@ -47,6 +41,124 @@ describe('LegacyFileReaderFactory', () => {
       );
       expect(processed.supportedFiles.length).toBe(0);
       expect(processed.unsupportedFiles).toEqual(unsupported ? [trace] : []);
+    }
+  });
+
+  describe('creates', () => {
+    let sfFile: TraceFile;
+    let wmTransitionsFile: TraceFile;
+
+    beforeAll(async () => {
+      sfFile = new TraceFile(
+        await getFixtureFile('traces/elapsed_timestamp/SurfaceFlinger.pb'),
+      );
+      wmTransitionsFile = new TraceFile(
+        await getFixtureFile(
+          'traces/elapsed_and_real_timestamp/wm_transition_trace.pb',
+        ),
+      );
+    });
+
+    it('InputMethodClients reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_timestamp/InputMethodClients.pb',
+        TraceType.INPUT_METHOD_CLIENTS,
+      );
+    });
+
+    it('InputMethodManagerService reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_timestamp/InputMethodManagerService.pb',
+        TraceType.INPUT_METHOD_MANAGER_SERVICE,
+      );
+    });
+
+    it('InputMethodService reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_timestamp/InputMethodService.pb',
+        TraceType.INPUT_METHOD_SERVICE,
+      );
+    });
+
+    it('ProtoLog reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_and_real_timestamp/ProtoLog32.pb',
+        TraceType.PROTO_LOG,
+      );
+    });
+
+    it('SurfaceFlinger reader', async () => {
+      await processFiles([sfFile], [TraceType.SURFACE_FLINGER]);
+    });
+
+    it('Transactions reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_timestamp/Transactions.pb',
+        TraceType.TRANSACTIONS,
+      );
+    });
+
+    it('WindowManager reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_timestamp/WindowManager.pb',
+        TraceType.WINDOW_MANAGER,
+      );
+    });
+
+    it('WindowManager dump reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_timestamp/dump_WindowManager.pb',
+        TraceType.WINDOW_MANAGER,
+      );
+    });
+
+    it('WM Transitions reader', async () => {
+      await processFiles([wmTransitionsFile], [TraceType.WM_TRANSITION]);
+    });
+
+    it('Shell Transitions reader', async () => {
+      await createsReaderForFile(
+        'traces/elapsed_and_real_timestamp/shell_transition_trace.pb',
+        TraceType.SHELL_TRANSITION,
+      );
+    });
+
+    it('ViewCapture readers', async () => {
+      const file = await getFixtureFile(
+        'traces/elapsed_and_real_timestamp/com.google.android.apps.nexuslauncher_0.vc',
+      );
+      await processFiles(
+        [new TraceFile(file)],
+        [TraceType.VIEW_CAPTURE, TraceType.VIEW_CAPTURE],
+      );
+    });
+
+    it('multiple readers of different types', async () => {
+      await processFiles(
+        [wmTransitionsFile, sfFile],
+        [TraceType.WM_TRANSITION, TraceType.SURFACE_FLINGER],
+      );
+    });
+
+    async function createsReaderForFile(filepath: string, type: TraceType) {
+      const file = new TraceFile(await getFixtureFile(filepath));
+      await processFiles([file], [type]);
+    }
+
+    async function processFiles(
+      files: TraceFile[],
+      types: TraceType[],
+      unsupportedFiles: TraceFile[] = [],
+    ) {
+      const processedFiles = await new LegacyFileReaderFactory().processFiles(
+        files,
+        UTC_CONVERTER,
+      );
+      expect(
+        processedFiles.supportedFiles.map((p) => p.getTraceType()),
+      ).toEqual(types);
+      expect(processedFiles.unsupportedFiles).toEqual(unsupportedFiles);
+      return processedFiles;
     }
   });
 });
---
+++ tools/winscope/src/app/perfetto_parser_factory_test.ts
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {getFixtureFile} from '@test/unit/io_helpers';
+import {UTC_CONVERTER} from '@test/unit/time_test_helpers';
+import {TraceFile} from '@trace/trace_file';
+import {TraceType} from '@trace_api/trace_type';
+import {PerfettoParserFactory} from './perfetto_parser_factory';
+import {UserNotifierChecker} from '@test/unit/user_notifier_checker';
+import {makeWarningInvalidPerfettoTrace} from '@parsers/helpers/warnings';
+
+describe('PerfettoParserFactory', () => {
+  describe('is robust to', () => {
+    it('invalid perfetto file', async () => {
+      const userNotifierChecker = new UserNotifierChecker();
+      await checkRobustToFile('invalid_files/invalid_protolog.perfetto-trace');
+      userNotifierChecker.expectAdded([
+        makeWarningInvalidPerfettoTrace('invalid_protolog.perfetto-trace', [
+          'Perfetto trace has no Winscope trace entries',
+        ]),
+      ]);
+    });
+
+    it('non-perfetto file', async () => {
+      await checkRobustToFile(
+        'traces/elapsed_timestamp/SurfaceFlinger.pb',
+        false,
+      );
+    });
+
+    it('empty perfetto file', async () => {
+      await checkRobustToFile(
+        'invalid_files/no_winscope_traces.perfetto-trace',
+      );
+    });
+
+    async function checkRobustToFile(filepath: string, isPerfettoTrace = true) {
+      const file = new TraceFile(await getFixtureFile(filepath));
+      const processed = await new PerfettoParserFactory().processFile(
+        file,
+        UTC_CONVERTER,
+      );
+      expect(processed.parsers.length).toBe(0);
+      expect(processed.isPerfettoTrace).toEqual(isPerfettoTrace);
+      expect(processed.traceGeometryData !== undefined).toEqual(
+        isPerfettoTrace,
+      );
+    }
+  });
+
+  describe('creates', () => {
+    it('IME readers', async () => {
+      await createsReaderForFile('traces/perfetto/ime.perfetto-trace', [
+        TraceType.INPUT_METHOD_CLIENTS,
+        TraceType.INPUT_METHOD_MANAGER_SERVICE,
+        TraceType.INPUT_METHOD_SERVICE,
+      ]);
+    });
+
+    it('ProtoLog reader', async () => {
+      await createsReaderForFile('traces/perfetto/protolog.perfetto-trace', [
+        TraceType.PROTO_LOG,
+      ]);
+    });
+
+    it('SurfaceFlinger reader', async () => {
+      await createsReaderForFile(
+        'traces/perfetto/layers_trace.perfetto-trace',
+        [TraceType.SURFACE_FLINGER],
+      );
+    });
+
+    it('Transactions reader', async () => {
+      await createsReaderForFile(
+        'traces/perfetto/transactions_trace.perfetto-trace',
+        [TraceType.TRANSACTIONS],
+      );
+    });
+
+    it('Transitions reader', async () => {
+      await createsReaderForFile(
+        'traces/perfetto/shell_transitions_trace.perfetto-trace',
+        [TraceType.TRANSITION],
+      );
+    });
+
+    it('ViewCapture reader', async () => {
+      await createsReaderForFile('traces/perfetto/viewcapture.perfetto-trace', [
+        TraceType.VIEW_CAPTURE,
+      ]);
+    });
+
+    it('WindowManager reader', async () => {
+      await createsReaderForFile(
+        'traces/perfetto/windowmanager.perfetto-trace',
+        [TraceType.WINDOW_MANAGER],
+      );
+    });
+
+    it('Input readers', async () => {
+      await createsReaderForFile(
+        'traces/perfetto/input-events.perfetto-trace',
+        [TraceType.INPUT_MOTION_EVENT, TraceType.INPUT_KEY_EVENT],
+      );
+    });
+
+    it('CUJ reader', async () => {
+      await createsReaderForFile('traces/perfetto/cujs.perfetto-trace', [
+        TraceType.CUJS,
+      ]);
+    });
+
+    async function createsReaderForFile(filepath: string, types: TraceType[]) {
+      const file = new TraceFile(await getFixtureFile(filepath));
+      await processFiles(file, types);
+    }
+
+    async function processFiles(file: TraceFile, types: TraceType[]) {
+      const processedFiles = await new PerfettoParserFactory().processFile(
+        file,
+        UTC_CONVERTER,
+      );
+      expect(processedFiles.parsers.map((p) => p.getTraceType())).toEqual(
+        types,
+      );
+      expect(processedFiles.isPerfettoTrace).toEqual(true);
+      expect(processedFiles.traceGeometryData).toBeDefined();
+    }
+  });
+});
