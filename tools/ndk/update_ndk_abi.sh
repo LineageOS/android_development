2c05ef0ae3f78dba599fe8e085375e7bf3f4b42f
Linus Tufvesson <lus@google.com>
2026-02-04 13:14:19 +0100

    [ndk] Improve ABI update tool for automation
    Soong isn't happy with an absolute path anymore. See https://paste.googleplex.com/5563118732312576
    
    - Make path relative to build_top
    - Add --skip-build flag to ndkabidump.
    - Honor TARGET_RELEASE from environment in soong.py.
    - Require TARGET_RELEASE in update_ndk_abi.sh.
    
    Bug: 478792558
    Test: ./development/tools/ndk/update_ndk_abi.sh --help
    Test: ./development/tools/ndk/update_ndk_abi.sh --skip-build
    Change-Id: Ifbba71e29178e32889b074791de5245bce37b5c2

--- tools/ndk/update_ndk_abi.sh
+++ tools/ndk/update_ndk_abi.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
+
+if [ -z "$TARGET_RELEASE" ]; then
+    echo "TARGET_RELEASE must be set. Either 'lunch' something or prepend your call like this 'TARGET_RELEASE=<release> $0'"
+    exit 1
+fi
+
 set -x
 
 THIS_DIR=$(dirname "$(realpath $0)")
 TOP=$(realpath $THIS_DIR/../../..)
 
-TARGET_RELEASE=trunk_staging $TOP/build/soong/soong_ui.bash --make-mode update-ndk-abi
+$TOP/build/soong/soong_ui.bash --make-mode update-ndk-abi
 $TOP/out/host/linux-x86/bin/update-ndk-abi --src-dir $TOP $TOP/ndk-abi-out "$@"
