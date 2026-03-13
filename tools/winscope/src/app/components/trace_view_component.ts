9389cc36c0726d3390ff587c590e1dab5f219a25
Priyanka Patel <priyankaspatel@google.com>
2026-03-13 13:42:47 +0000

    Inject viewer components in TraceViewComponent.
    Bug: 491756351
    Test: npm run test:unit:ci
    Change-Id: I22f38d81a8f8087924a4acf0444764d348a7de76

--- tools/winscope/src/app/components/trace_view_component.ts
+++ tools/winscope/src/app/components/trace_view_component.ts
@@ -16,7 +16,7 @@
 
 import {OverlayModule} from '@angular/cdk/overlay';
 import {CommonModule} from '@angular/common';
-import {ChangeDetectorRef, Component, computed, effect, ElementRef, Inject, input, NgZone, signal,} from '@angular/core';
+import {ApplicationRef, ChangeDetectorRef, Component, computed, createComponent, effect, ElementRef, Inject, input, NgZone, signal,} from '@angular/core';
 import {FormControl, ReactiveFormsModule, ValidationErrors, Validators,} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
@@ -37,12 +37,12 @@ import {EmitEvent, WinscopeEventEmitter,} from '@messaging/winscope_event_emitte
 import {WinscopeEventListener} from '@messaging/winscope_event_listener';
 import {TRACE_INFO} from '@trace_api/trace_info';
 import {TraceType} from '@trace_api/trace_type';
-import {View, Viewer, ViewType} from '@viewers/viewer';
+import {Viewer, ViewType} from '@viewers/viewer';
 
 interface Tab {
-  view: View;
   addedToDom: boolean;
   isTooltipStable: boolean;
+  viewer: Viewer;
 }
 
 /**
@@ -77,6 +77,7 @@ export class TraceViewComponent
 
   private allFilterPresets = signal<string[]>([]);
   private currentActiveTab = signal<Tab | undefined>(undefined);
+  private viewInitialized = signal(false);
 
   TRACE_INFO = TRACE_INFO;
   tabs: Tab[] = [];
@@ -99,7 +100,7 @@ export class TraceViewComponent
   );
 
   private getCurrentTabTraceType = computed<TraceType | undefined>(() => {
-    return this.currentActiveTab()?.view.traces.at(0)?.type;
+    return this.currentActiveTab()?.viewer.getTraces().at(0)?.type;
   });
 
   readonly currentTabHasFilterPresets = computed(() => {
@@ -131,11 +132,16 @@ export class TraceViewComponent
   traceTypesWithParsingErrorsWarningTooltip: string = '';
 
   constructor(
-    @Inject(ElementRef) private elementRef: ElementRef,
-    @Inject(ChangeDetectorRef) private changeDetectorRef: ChangeDetectorRef,
-    @Inject(NgZone) private ngZone: NgZone,
+    @Inject(ElementRef) private readonly elementRef: ElementRef,
+    @Inject(ChangeDetectorRef)
+    private readonly changeDetectorRef: ChangeDetectorRef,
+    @Inject(NgZone) private readonly ngZone: NgZone,
+    @Inject(ApplicationRef) private readonly applicationRef: ApplicationRef,
   ) {
     const firstViewersChange = effect(() => {
+      if (!this.viewInitialized()) {
+        return;
+      }
       const viewers = this.viewers();
       this.renderViewsTab(viewers);
       this.renderViewsOverlay(viewers);
@@ -152,19 +158,27 @@ export class TraceViewComponent
     });
   }
 
+  ngAfterViewInit() {
+    this.viewInitialized.set(true);
+  }
+
   getTabIconColor(tab: Tab): string {
-    if (tab.view.type === ViewType.GLOBAL_SEARCH) return '';
-    const trace = tab.view.traces.at(0);
-    if (!trace) return '';
+    if (tab.viewer.getViewType() === ViewType.GLOBAL_SEARCH) return '';
+    const trace = tab.viewer.getTraces().at(0);
+    if (!trace) {
+      return '';
+    }
     return TRACE_INFO[trace.type].color;
   }
 
   getTabIcon(tab: Tab): string {
-    if (tab.view.type === ViewType.GLOBAL_SEARCH) {
+    if (tab.viewer.getViewType() === ViewType.GLOBAL_SEARCH) {
       return TRACE_INFO[TraceType.SEARCH].icon;
     }
-    const trace = tab.view.traces.at(0);
-    if (!trace) return '';
+    const trace = tab.viewer.getTraces().at(0);
+    if (!trace) {
+      return '';
+    }
     return TRACE_INFO[trace.type].icon;
   }
 
@@ -205,17 +219,18 @@ export class TraceViewComponent
     return tab === this.currentActiveTab();
   }
 
-  getTabTooltip(view: View): string {
+  getTabTooltip(viewer: Viewer): string {
     const desc = new Set();
-    view.traces.forEach((trace) =>
-      trace.getDescriptors().forEach((d) => desc.add(d)),
-    );
+    viewer
+      .getTraces()
+      .forEach((trace) => trace.getDescriptors().forEach((d) => desc.add(d)));
     return Array.from(desc).join(', ');
   }
 
-  getTitle(view: View): string {
-    const isDump = view.traces.length === 1 && view.traces.at(0)?.isDump();
-    return view.title + (isDump ? ' Dump' : '');
+  getTitle(viewer: Viewer): string {
+    const traces = viewer.getTraces();
+    const isDump = traces.length === 1 && traces.at(0)?.isDump();
+    return viewer.getTitle() + (isDump ? ' Dump' : '');
   }
 
   onFilterPresetsClick() {
@@ -226,7 +241,9 @@ export class TraceViewComponent
   }
 
   async savePreset() {
-    if (this.filterPresetNameControl.invalid) return;
+    if (this.filterPresetNameControl.invalid) {
+      return;
+    }
     await this.ngZone.run(async () => {
       const value = assertDefined(this.filterPresetNameControl.value);
       const currentTabTraceType = assertDefined(this.getCurrentTabTraceType());
@@ -270,7 +287,7 @@ export class TraceViewComponent
   }
 
   showTraceTypesWithParsingErrorsWarning(tab: Tab): boolean {
-    const trace = tab.view.traces.at(0);
+    const trace = tab.viewer.getTraces().at(0);
     const traceType = trace?.type;
 
     if (traceType !== undefined) {
@@ -295,19 +312,17 @@ export class TraceViewComponent
 
   private async onTabbedViewSwitchRequest(event: TabbedViewSwitchRequest) {
     const tab = this.tabs.find((tab) =>
-      tab.view.traces.some((trace) => trace === event.newActiveTrace),
+      tab.viewer.getTraces().some((trace) => trace === event.newActiveTrace),
     );
     await this.showTab(assertDefined(tab), false);
   }
 
   private renderViewsTab(viewers: Viewer[]) {
     this.tabs = viewers
-      .map((viewer) => viewer.getViews())
-      .flat()
-      .filter((view) => view.type !== ViewType.OVERLAY)
-      .map((view) => {
+      .filter((viewer) => viewer.getViewType() !== ViewType.OVERLAY)
+      .map((viewer) => {
         return {
-          view,
+          viewer,
           addedToDom: false,
           isTooltipStable: false,
         };
@@ -315,19 +330,20 @@ export class TraceViewComponent
 
     if (this.tabs.length > 0) {
       const tabToShow = assertDefined(
-        this.tabs.find((tab) => tab.view.type !== ViewType.GLOBAL_SEARCH),
+        this.tabs.find(
+          (tab) => tab.viewer.getViewType() !== ViewType.GLOBAL_SEARCH,
+        ),
       );
       this.showTab(tabToShow, true);
     }
   }
 
   private renderViewsOverlay(viewers: Viewer[]) {
-    const views: View[] = viewers
-      .map((viewer) => viewer.getViews())
-      .flat()
-      .filter((view) => view.type === ViewType.OVERLAY);
+    const overlayViewers: Viewer[] = viewers.filter(
+      (viewer) => viewer.getViewType() === ViewType.OVERLAY,
+    );
 
-    if (views.length > 1) {
+    if (overlayViewers.length > 1) {
       throw new Error(
         'Only one overlay view is supported. To allow more overlay views, either create more than' +
           ' one draggable containers in this component or move the cdkDrag directives into the' +
@@ -336,44 +352,41 @@ export class TraceViewComponent
       );
     }
 
-    views.forEach((view) => {
-      view.htmlElement.style.pointerEvents = 'all';
+    overlayViewers.forEach((viewer) => {
       const container = assertDefined(
         this.elementRef.nativeElement.querySelector('.overlay-container'),
       );
-      container.appendChild(view.htmlElement);
+      this.injectComponent(viewer, container);
     });
   }
 
   private async showTab(tab: Tab, firstToRender: boolean) {
     const startTimeMs = Date.now();
     const currentActiveTab = this.currentActiveTab();
+    if (tab === currentActiveTab) {
+      return;
+    }
     if (currentActiveTab) {
-      currentActiveTab.view.htmlElement.style.display = 'none';
+      currentActiveTab.viewer.onHide();
     }
 
     const firstSwitch = !tab.addedToDom;
     if (firstSwitch) {
-      // Workaround for b/255966194:
-      // make sure that the first time a tab content is rendered
-      // (added to the DOM) it has style.display == "". This fixes the
-      // initialization/rendering issues with cdk-virtual-scroll-viewport
-      // components inside the tab contents.
       const traceViewContent = assertDefined(
         this.elementRef.nativeElement.querySelector('.trace-view-content'),
       );
-      traceViewContent.appendChild(tab.view.htmlElement);
+      this.injectComponent(tab.viewer, traceViewContent);
       tab.addedToDom = true;
     } else {
-      tab.view.htmlElement.style.display = '';
+      tab.viewer.onShow();
     }
 
     this.currentActiveTab.set(tab);
 
     if (!firstToRender) {
-      await this.emitAppEvent(new TabbedViewSwitched(tab.view));
+      await this.emitAppEvent(new TabbedViewSwitched(tab.viewer));
       Analytics.Navigation.logTabSwitched(
-        tab.view.title,
+        tab.viewer.getTitle(),
         Date.now() - startTimeMs,
         firstSwitch,
       );
@@ -381,6 +394,18 @@ export class TraceViewComponent
     if (firstSwitch) {
       Analytics.Memory.logUsage('tab_initialized', {firstSwitch});
     }
+  }
+
+  private injectComponent(viewer: Viewer, container: HTMLElement) {
+    const environmentInjector = this.applicationRef.injector;
+    const componentRef = createComponent(viewer.getComponentType(), {
+      environmentInjector,
+    });
+    this.applicationRef.attachView(componentRef.hostView);
+    viewer.setComponentRef(componentRef);
+    container.appendChild(componentRef.location.nativeElement);
+    viewer.onShow();
+    componentRef.changeDetectorRef.detectChanges();
   }
 
   private validateFilterPresetName(
