5569038f5c65fbaa43510015d354161339445464
Priyanka Patel <priyankaspatel@google.com>
2026-03-26 15:36:07 +0000

    Bring back active trace changes on tab click.
    Fixes: 496543198
    Test: npm run test:unit:ci
    Change-Id: I80e335e28446c90dec24a50467f1df8ed0eae07d

--- tools/winscope/src/app/trace_view_component.ts
+++ tools/winscope/src/app/trace_view_component.ts
@@ -33,6 +33,7 @@ import {Analytics} from '@logging/analytics';
 import {WinscopeEvent} from '@messaging/winscope_event';
 import {EmitEvent, WinscopeEventEmitter,} from '@messaging/winscope_event_emitter';
 import {WinscopeEventListener} from '@messaging/winscope_event_listener';
+import {ActiveTraceChanged} from '@trace_api/trace_events';
 import {TRACE_INFO} from '@trace_api/trace_info';
 import {TraceType} from '@trace_api/trace_type';
 import {FilterPresetApplyRequest, FilterPresetSaveRequest,} from '@ui/shared/events/misc_events';
@@ -365,6 +366,10 @@ export class TraceViewComponent
     const startTimeMs = Date.now();
     const currentActiveTab = this.currentActiveTab();
     if (tab === currentActiveTab) {
+      const trace = tab.viewer.getTraces().at(0);
+      if (trace) {
+        await this.emitAppEvent(new ActiveTraceChanged(trace));
+      }
       return;
     }
     if (currentActiveTab) {
--- tools/winscope/src/app/trace_view_component_test.ts
+++ tools/winscope/src/app/trace_view_component_test.ts
@@ -35,6 +35,7 @@ import {checkTooltips, DOMTestHelper} from '@common/testing/dom_test_helpers';
 import {makeZeroTimestamp} from '@common/time/testing/test_helpers';
 import {TraceBuilder} from '@trace_api/testing/trace_builder';
 import {makeEmptyTrace} from '@trace_api/testing/trace_test_helpers';
+import {ActiveTraceChanged} from '@trace_api/trace_events';
 import {TraceType} from '@trace_api/trace_type';
 import {HierarchyTreeNode} from '@tree_node/hierarchy_tree_node';
 import {FilterPresetApplyRequest, FilterPresetSaveRequest,} from '@ui/shared/events/misc_events';
@@ -367,13 +368,16 @@ describe('TraceViewComponent', () => {
     showTab1.calls.reset();
     hideTab0.calls.reset();
 
-    // Stay on tab 1
+    // Stay on tab 1, change active trace
+    const emitSpy = jasmine.createSpy();
+    component.setEmitEvent(emitSpy);
     await switchTab1();
     expect(getVisibleTabContents()).toEqual(['Content0', 'Content1']);
     expect(showTab0).not.toHaveBeenCalled();
     expect(showTab1).not.toHaveBeenCalled();
     expect(hideTab0).not.toHaveBeenCalled();
     expect(hideTab1).not.toHaveBeenCalled();
+    expect(emitSpy).toHaveBeenCalledOnceWith(new ActiveTraceChanged(traceWm));
 
     // Switch to tab 0
     await switchTab0();
